88c00295ee8695846d72ff355e5d53d729b10255
Ralph <02travis.ralph@gulllakecs.org>
2024-02-09 14:05:15 -0500

    Import Time Library - Called and Stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -1,3 +1,9 @@
 
-print ("\n\nWelcome to InfoTech Center V1.0")
-print ("\nInfoTech Center Operating System Loading...")
+#import libraries here
+import time
+
+timeToSleep = 1
+
+print("\n\nWelcome to InfoTech Center V1.0")
+time.sleep(timeToSleep)
+print("\nInfoTech Center Operating System Loading...")
