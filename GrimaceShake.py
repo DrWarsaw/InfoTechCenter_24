22f596c75ff4f0b6c722e333770ba465a3b7fae2
Ralph <02travis.ralph@gulllakecs.org>
2024-02-21 14:00:15 -0500

    Changed names and Defs of Variables - Called and stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -14,26 +14,26 @@ def gasLevelGauge():
 #funtions that lists gas station, m randomly choosing on and returning its value
 def listOfGasStations():
     gasStations = ["Shell", "Speedway", "Marathon", "Circle K","Moble", "Costco", "Mijer","7Eleven"]
-    gasStationsNearby = random.choice(gasStation)
+    gasStationsNearby = random.choice(gasStations)
     return  gasStationsNearby
 
 #funtion will call the gasLevelGage to determine our gas level and then find a close gas station
 # by calling the listOfGasStations function if we are on a low or quarter tank
 
-def gasLevelAlert():
+def gasStations():
     milesToGasOnLow = round(random.uniform(1, 25),1)
     milesToGasOnQT = round(random.uniform(25.1, 50),1)
     gasLevelPeeper= gasLevelGauge()
-    if gasLevel == "Empty":
+    if gasLevelPeeper == "Empty":
         print("*** WARNING - YOU ARE ON EMPTY")
         sleep(1.25)
         print("\n\n***Calling AAA***")
     elif gasLevelPeeper == "Low":
         print("Your gas tank is very low, checking google maps for the closest gas station.")
         sleep(2.5)
-        print("The closest gas station is",gasStations(),"which is",milesToGasStationLow,"miles away.")
+        print("The closest gas station is",listOfGasStations(),"which is",milesToGasOnLow,"miles away.")
 
-gasLevelAlert()
+gasStations()
 
 
  
